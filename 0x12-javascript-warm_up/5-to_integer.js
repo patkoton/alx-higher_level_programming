d99be6ba39305f9905b0574a4e81f6dd6dff3c16
patkoton <68227675+patkoton@users.noreply.github.com>
2022-10-04 22:10:49 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  console.log('Not a number');
+// Handle arguments with Javascript
+
+let parsed = parseInt(process.argv[2]);
+
+if (parsed) {
+  parsed = 'My number: ' + parsed;
+  console.log(parsed);
 } else {
-  console.log('My number:', parseInt(process.argv[2]));
+  console.log('Not a number');
 }
