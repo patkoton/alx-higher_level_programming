d893d1b50de36f3f52d4bf8337b883e7befbbd75
patkoton <68227675+patkoton@users.noreply.github.com>
2022-10-04 16:33:27 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-'use strict';
-let l = process.argv.length;
-if (l === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (l === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
